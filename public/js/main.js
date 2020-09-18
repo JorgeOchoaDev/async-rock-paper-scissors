e59c662ba17221fca4c7ef62d41467f7c0f4389d
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2020-09-17 23:11:33 -0500

    first player hand

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 let database = firebase.database();
 let gamesRef = database.ref("/games")
+let currentGame;
+let selectedOption;
 
 gamesRef.on("value", snapshot => {
     console.log(snapshot.val())
@@ -9,4 +11,18 @@ gamesRef.on("value", snapshot => {
 document.getElementById("new-game").addEventListener("click", ()=>{
     let gameKey = gamesRef.push({active: true}).key
     document.getElementById("game-key-val").innerHTML = `Tu id de juego es: ${gameKey}`
+    currentGame = gameKey
 })
+
+document.querySelectorAll(".option-radio").forEach( radio => {
+    console.log(radio)
+    radio.addEventListener("click", event => {
+        selectedOption = event.target.value
+        console.log(selectedOption)
+    })
+})
+
+document.getElementById("go-button").addEventListener("click", () => {
+    gamesRef.child(currentGame).update({ player1hand: selectedOption})
+})
+
